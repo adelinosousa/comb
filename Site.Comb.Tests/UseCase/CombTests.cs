64314e65a069a2038fecc085d3e0890baa95f740
Adelino Sousa <lino.sousa.86@gmail.com>
2019-04-22 07:09:16 +0100

    Added base unit tests

---
+++ Site.Comb.Tests/UseCase/CombTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site.Comb.Tests
+{
+    [TestClass]
+    public class CombTests
+    {
+        private ICombHttpClient combHttpClient;
+        private Comb comb;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.combHttpClient = Substitute.For<ICombHttpClient>();
+            this.comb = new Comb(combHttpClient);
+        }
+
+        [TestMethod]
+        public async Task CombWithNullOrEmptyUrl()
+        {
+            var response = await this.comb.Brush(new CombRequest
+            {
+                Url = null
+            });
+
+            Assert.IsFalse(response.Success);
+            Assert.IsTrue(response.Errors.Count == 1);
+            Assert.AreEqual(response.Errors.First(), "Invalid Url");
+        }
+    }
+}
